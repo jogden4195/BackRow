3739d03a60fb89d02046add4a6e9b3b7db2da565
Jinji <jinji.zhang@github.com>
2019-03-13 16:28:13 -0700

    Include landings app in backrow urls

--- project/src/backrow/urls.py
+++ project/src/backrow/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from landing import views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home'),
+    path('', include('landing.urls')),
 ]

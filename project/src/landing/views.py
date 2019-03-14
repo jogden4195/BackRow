644662648b391f9d413a262ca6fb82d6cf95249e
thenicopixie <391@holbertonschool.com>
2019-03-14 14:56:10 -0700

    add ulr and view for presentation path

--- project/src/landing/views.py
+++ project/src/landing/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import requests
 
 
 def home(request):
@@ -10,3 +11,10 @@ def picker(request):
 
 def audience(request):
     return render(request, 'landing/audience.html')
+
+def presentation(request, fileId):
+    # return HttpResponse('<h1>Your fileID is ' + fileId + '! </h1>')
+    req = requests.get('https://www.googleapis.com/drive/v2/files/{}'.format(fileId))
+    if req.status_code == 200:
+        elink = req.json().get('embedLink')
+    return render(request, 'landing/presentation.html', {'elink': elink})

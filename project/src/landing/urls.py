68fac8cacf44ba51fad76ad0ac9e303642c5d697
Jenn <jennogden95@gmail.com>
2019-03-17 13:48:57 -0700

    Implemented About page

--- project/src/landing/urls.py
+++ project/src/landing/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.home, name='landing-home'),
     path('picker/', views.picker, name='picker'),
     path('audience/', views.audience, name='audience-home'),
-    path('presentation/<str:fileId>/', views.presentation, name='presentation-page')
+    path('presentation/<str:fileId>/', views.presentation, name='presentation-page'),
+    path('about', views.about, name='about'),
 ]
